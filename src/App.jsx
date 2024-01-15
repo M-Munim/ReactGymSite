a31d90a2291cdbbf0966665765418252d1fa30d0
munim <muhammadmunimoff330@gmail.com>
2024-01-15 23:04:25 +0500

    basic file strucure

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Home from "./pages/Home/Home"
+import About from "./pages/About/About"
+import Contact from "./pages/Contact/Contact"
+import Gallery from "./pages/Gallery/Gallery"
+import Plans from "./pages/Plans/Plans"
+import Trainers from "./pages/Trainers/Trainers"
+import NotFound from "./pages/NotFound/NotFound"
+
+
+function App() {
+
+  return (
+    <>
+      <h1>HI</h1>
+      <Home></Home>
+      <About></About>
+      <Contact></Contact>
+      <Gallery></Gallery>
+      <Plans></Plans>
+      <Trainers></Trainers>
+      <NotFound></NotFound>
+    </>
+  )
+}
+
+export default App
